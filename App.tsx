211a26c19b503c00cb03ec64309352951bcaf382
jfernandesdev <jfernandes.dev@gmail.com>
2022-12-16 17:56:55 -0300

    CHORE: styled components configuration, theme definition with color tokens, font family and font sizes

--- App.tsx
+++ App.tsx
@@ -1,7 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import { ActivityIndicator } from 'react-native'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import theme from './src/theme'
+
 import { Groups } from '@screens/Groups'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
-   <Groups />
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+    </ThemeProvider>
   );
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,10 +1,13 @@
-import { StyleSheet } from 'react-native'
+import styled from 'styled-components/native'
 
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-})
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background: ${({theme}) => theme.COLORS.GRAY_600};
+`
+
+export const Title = styled.Text`
+  color: #FFF;
+  font-size: 48px;
+`
