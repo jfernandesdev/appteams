8f1e51f3486c2fa91947b4a91f473e4dd1d3ac8f
jfernandesdev <jfernandes.dev@gmail.com>
2022-12-17 08:59:36 -0300

    FEAT: header component

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,12 +2,5 @@ import styled from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
   background: ${({theme}) => theme.COLORS.GRAY_600};
-`
-
-export const Title = styled.Text`
-  color: #FFF;
-  font-size: 48px;
 `
