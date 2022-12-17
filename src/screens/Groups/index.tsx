4255e5b2e787a2d0ea91920a01d8c4ef4c242d32
jfernandesdev <jfernandes.dev@gmail.com>
2022-12-17 09:30:50 -0300

    FEAT: highlight component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,17 @@
 import { Header } from '@components/Header'
+import { Hightlight } from '@components/Hightlight'
+
 import { Container } from './styles'
 
 export function Groups() {
   return(
     <Container>
       <Header />
+
+      <Hightlight 
+        title="Turmas"
+        subtitle="jogue com sua turma"
+      />
     </Container>
   )
 }
