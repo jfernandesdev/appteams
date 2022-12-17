c7bc151c5f71c032bca1ad928487667ada5da25b
jfernandesdev <jfernandes.dev@gmail.com>
2022-12-17 07:51:46 -0300

    STYLE: loading component and status bar styling

--- App.tsx
+++ App.tsx
@@ -1,17 +1,25 @@
 import { ThemeProvider } from 'styled-components'
-import { ActivityIndicator } from 'react-native'
+import { StatusBar } from 'react-native'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import theme from './src/theme'
 
+import { Loading } from '@components/Loading'
 import { Groups } from '@screens/Groups'
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
