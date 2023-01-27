b71fc8f93e36fd183493ee6c078d51c63a70bff6
jfernandesdev <jfernandes.dev@gmail.com>
2023-01-27 09:25:15 -0300

    FEAT: opening classes and storing players within a team

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import { FlatList } from 'react-native'
+import { Alert, FlatList } from 'react-native'
 import { useRoute } from '@react-navigation/native'
+import { AppError } from '@utils/AppError'
 
 import { Header } from '@components/Header'
 import { Hightlight } from '@components/Hightlight'
@@ -11,18 +12,45 @@ import { PlayerCard } from '@components/PlayerCard'
 import { ListEmpty } from '@components/ListEmpty'
 import { Button } from '@components/Button'
 
+import { playerAddByGroup } from '@storage/player/playerAddByGroup'
+
 import { Container, Form, HeaderList, NumberOfPlayers } from './styles'
+import { playersGetByGroup } from '@storage/player/playersGetByGroup'
 
 type RouteParams = {
   group: string
 }
 
 export function Players() {
+  const [newPlayerName, setNewPlayerName] = useState('')
   const [team, setTeam] = useState('Time A')
   const [players, setPlayers] = useState([])
 
   const route = useRoute()
   const { group } = route.params as RouteParams
+
+  async function handleAddPlayer() {
+    if(newPlayerName.trim().length === 0) {
+     return Alert.alert('Nova pessoa', 'Informe o nome da pessoa para adicionar.');
+    }
+
+    const newPlayer = {
+      name: newPlayerName,
+      team,
+    }
+
+    try { 
+      await playerAddByGroup(newPlayer, group);
+      
+    } catch (error) {
+      if(error instanceof AppError) {
+        Alert.alert('Nova pessoa', error.message);
+      } else {
+        console.log(error);
+        Alert.alert('Nova pessoa', 'Não foi possível adicionar.')
+      }
+    }
+  }
 
   return(
     <Container>
@@ -35,11 +63,15 @@ export function Players() {
 
       <Form>
         <Input 
+          onChangeText={setNewPlayerName}
           placeholder="Nome da pessoa"
           autoCorrect={false}
         />
 
-        <ButtonIcon icon="add" />
+        <ButtonIcon
+          onPress={handleAddPlayer} 
+          icon="add" 
+        />
       </Form>
 
       <HeaderList>
--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,3 +1,4 @@
 const GROUP_COLLECTION = '@app-teams:groups'
+const PLAYER_COLLECTION = '@app-teams:players'
 
-export { GROUP_COLLECTION }
+export { GROUP_COLLECTION, PLAYER_COLLECTION }
